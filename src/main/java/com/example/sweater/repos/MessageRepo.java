c18355b656296e38a74b841b88fa280f026c9d7a
Dmitry <spez.dm@gmail.com>
2020-09-30 17:27:15 +0300

    sweater

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,6 @@
+package com.example.sweater.repos;
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+}
