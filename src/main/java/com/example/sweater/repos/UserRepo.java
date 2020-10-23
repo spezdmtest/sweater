96a5a78e7a06b6e2217b2b570985aefb229a29cc
Dmitry <spez.dm@gmail.com>
2020-10-23 17:48:54 +0300

    sweater

--- src/main/java/com/example/sweater/repos/UserRepo.java
+++ src/main/java/com/example/sweater/repos/UserRepo.java
@@ -4,5 +4,7 @@ import com.example.sweater.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User,Long> {
-    User findByUsername(String username); 
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
 }
--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -7,13 +7,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private MailSender mailSender;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
@@ -25,7 +31,27 @@ public class UserService implements UserDetailsService {
         }
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s! \n" + "Welcome to  Sweater. Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getEmail(),"Activation code",message);
+        }
         return true;
+    }
+
+    public boolean activateUser(String code) {
+       User user =  userRepo.findByActivationCode(code);
+
+       if(user != null) {
+           return false;
+       }
+       user.setActivationCode(null);
+       userRepo.save(user);
+       return true;
     }
 }
